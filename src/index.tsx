d4aaa8c3e98f4a7ec9537fae01ba2f9ffe67b4ba
herve <herve@ruault.org>
2019-07-14 19:31:01 +0200

    rename

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { render } from "react-dom";
 import "./styles.css";
 
-import { RecursiveItem } from "./ReactiveItem";
+import { RecursiveItem } from "./RecursiveItem";
 
 
   // #################################################################
